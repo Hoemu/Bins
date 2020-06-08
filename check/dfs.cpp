50dc24947367d5370ccda4841a81345652f16af3
Ray-ye <251590635@qq.com>
2020-06-08 10:25:03 +0800

    ent

--- check/dfs.cpp
+++ check/dfs.cpp
@@ -14,22 +14,23 @@ int main(){
 	vector<vector<int>>fl={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 	vector<vector<int>>fg=fl;
 	vector<vector<int>>sec={{0,-1},{0,1},{1,0},{-1,0}};
+	cout<<"ff"<<endl;
 	for(int i=0;i<fl.size();i++){
-		cout<<i<<" ";
+		//cout<<sum<<endl;
 		for(int j=0;j<fl[0].size();j++){
 			sum=0;
 			for(int tp=0;tp<5;tp++){
 				int x=i;
 				int y=j;
 				for(int k=0;k<4;k++){
-					if(x<0||y<0||x==3||y==4)break;
+
 					x+=sec[k][0];
 					y+=sec[k][1];
+					if(x<0||y<0||x==3||y==4)break;
 					if(fg[x][y]==0){
-						tp--;
 						continue;
 					}
-					cout<<sum<<" ";
+					//cout<<sum<<" ";
 					sum+=fg[x][y];
 					fg[x][y]=0;
 				}		
@@ -38,5 +39,7 @@ int main(){
 			fg=fl;
 		}
 	}
+	cout<<sums.size()<<endl;
+	//for(auto e:sums)cout<<e<<" ";
 	return 0;
 }
